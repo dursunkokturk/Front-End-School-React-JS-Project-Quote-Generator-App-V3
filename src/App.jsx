426b179aaeb765f5fd0d1e1f0fcbabcdf385c500
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-12 13:13:20 +0300

    src klasoru icindeki app.jsx component icindeki yazida duzenleme yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <>
       <div className="container">
-        <h6>Tavsiye #117</h6>
+        <h6>TAVSİYE #117</h6>
         <h2>“Dikkatini verip farkına varmak kolaydır, zor olan ise ayağa kalkıp harekete geçmektir.”</h2>
       </div>
     </>
